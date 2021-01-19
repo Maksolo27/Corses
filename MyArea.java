ee212711e006bcf41de099edb4e0471f4c9e4d03
Maksolo27 <71383659+Maksolo27@users.noreply.github.com>
2021-01-19 21:19:53 +0300

    Add files via upload

---
+++ MyArea.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MyArea {
+    private static double PI = 3.14;
+    static double areaOfCircle(double radius){
+        double res = (radius * radius) * PI;
+        return res;
+    }
+
+
+}
