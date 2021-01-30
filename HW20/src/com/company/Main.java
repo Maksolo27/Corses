748f1861104b1ce44c7f126998568ea83cbd7136
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-01-30 22:29:37 +0300

    Add files via upload

---
+++ HW20/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Vehicle.Wheel wheel = new Vehicle.Wheel();
+	    Vehicle.Door door = new Vehicle.Door();
+	    door.print();
+	    wheel.print();
+    }
+}
---
+++ HW20/src/com/company/Vehicle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Vehicle {
+    static class Wheel{
+        void print(){
+            System.out.println("Колесо");
+        }
+    }
+    static class Door{
+        void print(){
+            System.out.println("Дверь");
+        }
+
+    }
+}
