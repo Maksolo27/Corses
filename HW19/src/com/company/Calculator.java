fdda956f754a11ffb444a544f244fbe23d821acc
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-01-30 17:13:16 +0300

    Add files via upload

---
+++ HW19/src/com/company/Calculator.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Calculator {
+    static int add(int a, int b){
+        return a + b;
+    }
+    static int mult(int a, int b){
+        return a * b;
+    }
+    static int div(int a, int b){
+        return a / b;
+    }
+    static int subt(int a, int b) {
+        return a - b;
+    }
+}
---
+++ HW19/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(3,2));
+        System.out.println(Calculator.div(5,2));
+        System.out.println(Calculator.mult(6,2));
+        System.out.println(Calculator.subt(12, 4));
+    }
+}
