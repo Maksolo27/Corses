986844df1a741446d7933d081e0a7c3090758747
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-02-11 21:50:09 +0300

    Update Main.java

--- HW32/src/com/company/Main.java
+++ HW32/src/com/company/Main.java
@@ -1,17 +1,25 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Map<String, String> map = new HashMap<>();
 
         map.put("Москва", "Ивановы");
         map.put("Киев", "Петровы");
         map.put("Лондон", "Абрамовичи");
 
-        System.out.println(map.get("Лондон"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите ключ");
+        String current = reader.readLine();
+
+        System.out.println(map.get(current));
     }
 }
