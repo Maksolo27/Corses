ae4e7a80039e5c026b81c3f365d1982badf95ab0
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-02-16 20:45:53 +0200

    Add files via upload

---
+++ HW40/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String login = sc.nextLine();
+        sc = new Scanner(System.in);
+        String password = sc.nextLine();
+
+        Pattern pattern = Pattern.compile("[A-Za-z]+");
+        Matcher matcher = pattern.matcher(login);
+
+        boolean res = matcher.matches();
+        System.out.println(res);
+        Pattern pattern1 = Pattern.compile("\\w+");
+        Matcher matcher1 = pattern1.matcher(password);
+
+        boolean res1 = matcher1.matches();
+        System.out.println(res1);
+
+
+        if(res && res1){
+            System.out.println("Пароль и логин введены верно!");
+        }
+
+
+    }
+}
