b60f6ac829c54b366d84032565cbb60a32862bfe
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-02-17 22:12:22 +0200

    Add files via upload

--- HW42/src/com/company/Main.java
+++ HW42/src/com/company/Main.java
@@ -8,29 +8,29 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         File file = new File("C:\\Users\\maxim\\IdeaProjects\\HW42\\src\\text.txt");
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String text = "";
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String readText = "";
         String temp;
-        while ( (temp = br.readLine()) != null) {
-            text += temp;   //Считываю файл
+        while ( (temp = reader.readLine()) != null) {
+            readText += temp;   //Считываю файл
         }
-        String[] array = text.split(" ");   //Разбиваю текст по словам в массив
-        String result = "";
+        String[] array = readText.split(" ");   //Превращаю текст в массив
+        String matchedText = "";
         for (int i = 0; i < array.length; i++) {
             String regex = "\\w{4}";
             Pattern p = Pattern.compile(regex);
             Matcher m = p.matcher(array[i]);
             if(m.matches()){
-                array[i] = "Java ";     //Заменяю слова с длинной в 4 символа
+                array[i] = "Java";     //Заменяю слова с длинной в 4 символа
             }
-            result += array[i] + " "; //Добавляю пробел к каждому слову
+            matchedText += array[i] + " "; //Добавляю пробел к каждому слову
         }
-        String res = text.replaceAll(text,result);  //Заменяю на новый текст
-        br.close();
-        file.delete();               //Удаляю файл и создаю новый, чтобы заполнить замененным текстом
+        String replacedText = readText.replaceAll(readText,matchedText);  //Заменяю на новый текст
+        reader.close();
+        file.delete();               //Удаляю файл и создаю новый, чтобы заменеть текст
         file = new File("C:\\Users\\maxim\\IdeaProjects\\HW42\\src\\text.txt");
-        PrintWriter pw = new PrintWriter(new FileWriter(file, true));
-        pw.println(res);
-        pw.close();
+        PrintWriter writer = new PrintWriter(new FileWriter(file, true));
+        writer.println(replacedText);
+        writer.close();
     }
 }
