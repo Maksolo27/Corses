567f08db66f0150e6a2a3fd7108c588c996c7a86
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-02-18 17:51:40 +0200

    Add files via upload

---
+++ HW43/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите дату: ");
+        String date = reader.readLine(); //Строка не только с числами и спец.символами
+        String[] array = date.split(""); //Массив с числами и спец.символами
+        String number = ""; //Строка только с числами
+        for (int i = 0; i < array.length; i++) {
+            Pattern p = Pattern.compile("\\D");
+            Matcher m = p.matcher(array[i]);
+            if(m.matches()){
+                array[i] = "";      // Заменяю все числа на пустоту
+            }
+            number += array[i];
+        }
+        String[] numbers = number.split("");   // Создаю массив только чисел
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += Integer.valueOf(numbers[i]); //Беру значение строки
+        }
+        System.out.println(sum);
+
+
+    }
+}
