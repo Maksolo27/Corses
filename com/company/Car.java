be9581937fec6bb5abd6a3749c1680e4fb631a10
Maksolo27 <71383659+Maksolo27@users.noreply.github.com>
2021-03-11 16:52:05 +0300

    test

---
+++ com/company/Car.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Car {
+    int year;
+    String colour;
+    Car(){
+
+    };
+    Car(int year)
+    {
+        this.year = year;
+    }
+    Car(int year, String color){
+        this.year = year;
+        this.colour = color;
+    }
+
+
+}
